9852d45a263ee2b055d2e56848d13380d45aec4d
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-01-05 06:27:23 +0100

    SET: Basic routing with React

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import { HashRouter, Switch, Route } from "react-router-dom";
 import "./styles/app.css";
 
-// start the Stimulus application
 import "./bootstrap";
 import Navbar from "./js/components/NavBar";
 import HomePage from "./js/pages/HomePage";
+import CustomersPage from './js/pages/CustomersPage';
 
 const App = () => {
   return (
-    <>
+    <HashRouter>
       <Navbar />
 
-      <div className="container pt-5">
-        <HomePage />
-      </div>
-    </>
+      <main className="container pt-5">
+          <Switch>
+              <Route path="/customers" component={CustomersPage} />
+              <Route path="/" component={HomePage} />
+          </Switch>
+      </main>
+    </HashRouter>
   );
 };
 
