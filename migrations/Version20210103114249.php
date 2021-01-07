92d9766fcfa9ee0f6d41cc63fccf8b9f7b1594c0
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-01-07 12:07:41 +0100

    UPDATE: Remove doctrine command line on composer.json

--- migrations/Version20210103114249.php
+++ migrations/Version20210103114249.php
@@ -12,12 +12,12 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210103114249 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -26,7 +26,7 @@ final class Version20210103114249 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_81398E09A76ED395 ON customer (user_id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE customer DROP FOREIGN KEY FK_81398E09A76ED395');
