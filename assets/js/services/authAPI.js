b97d4d11736d47ad6592a4e22bb1743d610f95c6
AlbanRenahy <albanrenahy.pro@gmail.com>
2021-01-07 10:55:58 +0100

    REFACTORING: Centralize api config in one file

--- assets/js/services/authAPI.js
+++ assets/js/services/authAPI.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import { LOGIN_API } from "../config";
 
 // Remove token from localStorage and axios
 function logout() {
@@ -14,7 +15,7 @@ function logout() {
  */
 function authenticate(credentials) {
   return axios
-    .post("https://127.0.0.1:8001/api/login_check", credentials)
+    .post(LOGIN_API, credentials)
     .then((response) => response.data.token)
     .then((token) => {
       // Stock token in local storage
--- assets/js/services/customersAPI.js
+++ assets/js/services/customersAPI.js
@@ -1,27 +1,28 @@
 import axios from "axios";
+import { CUSTOMERS_API } from "../config";
 
 function findAll() {
   return axios
-    .get("https://127.0.0.1:8001/api/customers")
+    .get(CUSTOMERS_API)
     .then((response) => response.data["hydra:member"]);
 }
 
 function deleteCustomer(id) {
-  return axios.delete("https://127.0.0.1:8001/api/customers/" + id);
+  return axios.delete(CUSTOMERS_API + "/" + id);
 }
 
 function find(id) {
   return axios
-    .get("https://127.0.0.1:8001/api/customers/" + id)
+    .get(CUSTOMERS_API + "/" + id)
     .then((response) => response.data);
 }
 
 function update(id, customer) {
-  return axios.put("https://127.0.0.1:8001/api/customers/" + id, customer);
+  return axios.put(CUSTOMERS_API + "/" + id, customer);
 }
 
 function create(customer) {
-  axios.post("https://127.0.0.1:8001/api/customers", customer);
+  axios.post(CUSTOMERS_API, customer);
 }
 
 export default {
